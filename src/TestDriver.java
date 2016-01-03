97c9c804864e8fca32758d98e0910e8b0907e6ab
mdailerian <martin@dailerian.com>
2016-01-03 15:12:45 -0500

    added search

--- src/TestDriver.java
+++ src/TestDriver.java
@@ -5,22 +5,28 @@
 //import com.fasterxml.jackson.core.JsonFactory;
 //import com.fasterxml.jackson.core.JsonParser;
 //import jdk.nashorn.internal.ir.ObjectNode;
+
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.xcontent.XContentBuilder;
-//import org.elasticsearch.index.mapper.object.ObjectMapper;
-//import org.elasticsearch.node.Node;
-//import org.elasticsearch.node.NodeBuilder;
+import org.elasticsearch.search.SearchHit;
 
-//import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Date;
+import java.util.Map;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import static org.elasticsearch.index.query.QueryBuilders.*;
+
+//import org.elasticsearch.index.mapper.object.ObjectMapper;
+//import org.elasticsearch.node.Node;
+//import org.elasticsearch.node.NodeBuilder;
+//import java.io.File;
 //            http://teknosrc.com/elasticsearch-java-api-get-index-list/
 //            http://teknosrc.com/execute-raw-elasticsearch-query-using-transport-client-java-api/
 //            http://www.javacodegeeks.com/2013/04/getting-started-with-elasticsearch.html
@@ -41,6 +47,28 @@ public class TestDriver {
 
         return response;
     }
+
+    public static void searchDocument(Client client, String index, String type,
+                                      String field, String value){
+
+        SearchResponse response = client.prepareSearch(index)
+                .setTypes(type)
+                .setSearchType(SearchType.QUERY_AND_FETCH)
+                .setQuery(termQuery(field, value))//fieldQuery(field, value))
+                .setFrom(0).setSize(60).setExplain(true)
+                .execute()
+                .actionGet();
+
+        SearchHit[] results = response.getHits().getHits();
+
+        System.out.println("Current results: " + results.length);
+        for (SearchHit hit : results) {
+            System.out.println("------------------------------");
+            Map<String,Object> result = hit.getSource();
+            System.out.println(result);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println( "Driver Started...");
 
@@ -49,20 +77,18 @@ public class TestDriver {
             client = TransportClient.builder().build()
                     .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
 
-            System.out.println( "Client " + client.toString());
-
-
+            // build a dummy document to index in ES
             XContentBuilder doc = jsonBuilder()
                         .startObject()
                         .field("job", "driver")
                         .field("message", "Be smart")
                         .endObject();
-            System.out.println( doc );
 
             TestDriver td = new TestDriver();
-            IndexResponse response = td.indexDocument(client, doc );
-            System.out.println(" Response : " + response );
+        //    IndexResponse response = td.indexDocument(client, doc );
+        //    System.out.println(" Response : " + response );
 
+            td.searchDocument( client, "dailema2", "test", "job", "driver");
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
