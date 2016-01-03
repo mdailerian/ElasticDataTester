ec4b14ef644f4a5ae51542ba03b5ad022e5d946f
mdailerian <martin@dailerian.com>
2016-01-03 14:22:54 -0500

    minor refactoring

--- src/TestDriver.java
+++ src/TestDriver.java
@@ -2,22 +2,50 @@
 /**
  * Created by MMAA-local on 12/23/2015.
  */
+//import com.fasterxml.jackson.core.JsonFactory;
+//import com.fasterxml.jackson.core.JsonParser;
+//import jdk.nashorn.internal.ir.ObjectNode;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.node.Node;
-import org.elasticsearch.node.NodeBuilder;
+//import org.elasticsearch.index.mapper.object.ObjectMapper;
+//import org.elasticsearch.node.Node;
+//import org.elasticsearch.node.NodeBuilder;
 
+//import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Date;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+//            http://teknosrc.com/elasticsearch-java-api-get-index-list/
+//            http://teknosrc.com/execute-raw-elasticsearch-query-using-transport-client-java-api/
+//            http://www.javacodegeeks.com/2013/04/getting-started-with-elasticsearch.html
+//            http://docs.spring.io/spring-data/elasticsearch/docs/1.0.0.M1/reference/html/elasticsearch.repositories.html
+//      http://stackoverflow.com/questions/33909389/how-to-write-code-for-search-in-elasticsearch-using-jest-client-in-java
+
+//SEE RESULTS: examine sample data with Sense - http://localhost:5601/app/sense
+// JAVADOCS: http://javadoc.kyubu.de/elasticsearch/HEAD/allclasses-noframe.html
 
 public class TestDriver {
-// http://stackoverflow.com/questions/33909389/how-to-write-code-for-search-in-elasticsearch-using-jest-client-in-java
+
+    public IndexResponse indexDocument( Client client) throws IOException{
+        // create an index, stick some data
+            IndexResponse response = client.prepareIndex("dailema2", "test" )
+                    .setSource(jsonBuilder()
+                                    .startObject()
+                                    .field("user", "jenga")
+                                    .field("postDate", new Date())
+                                    .field("message", "Document7")
+                                    .endObject()
+                    )
+                    .execute()
+                    .actionGet();
+
+        return response;
+    }
     public static void main(String[] args) {
         System.out.println( "Driver Started...");
 
@@ -27,23 +55,15 @@ public class TestDriver {
                     .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
 //                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("host2"), 9300));
 
-            System.out.println( "Client " + client );
 
-            // create an index, stick some data
-            IndexResponse response = client.prepareIndex("twitter", "tweet", "1")
-                    .setSource(jsonBuilder()
-                                    .startObject()
-                                    .field("user", "kimchy")
-                                    .field("postDate", new Date())
-                                    .field("message", "trying out Elastic     Search")
-                                    .endObject()
-                    )
-                    .execute()
-                    .actionGet();
+            System.out.println( "Client " + client.toString());
 
-            // query - basic example
-            // analysis
-            // suggestions
+            //JsonParser parser = new JsonFactory()
+            //        .createParser(new File("C:\\Users\\MMAA-local\\Desktop\\Marti dev\\moviedata.json"));
+//            System.out.println( "JSON File " + parser );
+
+            TestDriver td = new TestDriver();
+            response = td.indexDocument( client );
 
         } catch (UnknownHostException e) {
             e.printStackTrace();
